794aa0c3cf6b949cb94c839d315967b2122f13c5
johntapuyohs <john.tapuyo@hypersense-software.com>
2023-09-09 22:34:45 +0800

    upload image test success

--- lib/ui/consultation/image.php
+++ lib/ui/consultation/image.php
@@ -1,28 +1,55 @@
-<?php 
-$return["error"] = false;
-$return["msg"] = "";
-//array to return
+<html>
+<head>
+    <title>Upload Base64 File</title>
+</head>
+<body>
+    <form action="image.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="base64_file" accept=".png" />
+        <input type="submit" value="Upload" />
+    </form>
+</body>
+</html>
 
-if(isset($_POST["image"])){
-    $base64_string = $_POST["image"];
-    $outputfile = "uploads/image.jpg" ;
-    //save as image.jpg in uploads/ folder
+<?php
 
-    $filehandler = fopen($outputfile, 'wb' ); 
-    //file open with "w" mode treat as text file
-    //file open with "wb" mode treat as binary file
-    
-    fwrite($filehandler, base64_decode($base64_string));
-    // we could add validation here with ensuring count($data)>1
 
-    // clean up the file resource
-    fclose($filehandler); 
-}else{
-    $return["error"] = true;
-    $return["msg"] =  "No image is submited.";
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//    if (isset($_FILES["base64_file"]) && isset($_POST["base64_data"])) {
+       $file_name = $_POST["file_name"];
+        $base64_data = $_POST["base64_data"];
+
+        // var_dump($_FILES);
+        // var_dump($_POST);
+
+
+        // Remove the data URI scheme (e.g., "data:image/png;base64,")
+        $base64_data = preg_replace('/^data:[^,]+,/', '', $base64_data);
+
+        // Decode the base64 data and save it to a file
+        $file_contents = base64_decode($base64_data);
+
+        if ($file_contents !== false) {
+            if (!file_exists($upload_dir)) {
+                mkdir($upload_dir, 0777, true);
+            }
+            
+            $file_path = "uploads/$file_name";
+
+            if (file_put_contents($file_path, $file_contents) !== false) {
+                echo "File uploaded successfully. $file_path";
+            } else {
+                echo "Failed to save the file. $file_path";
+            }
+        } else {
+            echo "Invalid base64 data.";
+        }
+    // } else {
+    //     echo "File or base64 data not provided.";
+    // }
+} else {
+    echo "Invalid request method.";
 }
-
-header('Content-Type: application/json');
-// tell browser that its a json data
-echo json_encode($return);
-//converting array to JSON string
